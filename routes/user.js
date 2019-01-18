604780a8e11706900f145910be2347e03868c877
Muhammad Zeeshan Khan <zeeshankhanaptech@gmail.com>
2019-01-18 19:53:18 +0500

    updated

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,5 @@ const router= express.Router();
 
 router.use("/",(re,res)=>console.log("add"));
 
-router.use("/del",(re,res)=>console.log("delete"));
-
-router.use("/up",(re,res)=>console.log("update"));
 
 module.exports=router;
