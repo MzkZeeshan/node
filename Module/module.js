71ab98f26dbe962c58723f2d3ea4d50a2085e7e4
Muhammad Zeeshan Khan <zeeshankhanaptech@gmail.com>
2019-01-19 20:37:33 +0500

    'done'

--- Module/module.js
+++ Module/module.js
@@ -1,6 +1,15 @@
-const mongoose= require("mongoose");
+// const mongoose= require("mongoose");
+// const data = new mongoose.Schema({
+//     toDo: String
+//   });
+//  const TodoData = mongoose.model('To-do', data);
+//  module.exports=TodoData;
+
+
+ const mongoose = require("mongoose");
 const data = new mongoose.Schema({
-    toDo: String
+  email:String,
+  psd:{type : String, unique : true}
   });
  const TodoData = mongoose.model('To-do', data);
  module.exports=TodoData;
--- routes/add.js
+++ routes/add.js
@@ -1,13 +1,48 @@
 const express=require("express");
 const router=express.Router();
 const Tododata = require("../Module/module")
+// const bcrypt = require("bcrypt")
+function hashPassword()
+{
+    var salt = bcrypt.genSaltSync(saltRounds);
+var hash = bcrypt.hashSync(Password, salt);
+}
+
+
 router.post('/', (req,res)=>
-{ console.log("add");
-const add=new Tododata(req.body)
+{ console.log("register");
+
+// const pHash = hashPassword(req.body.psd);
+
+
+const add=new Tododata({
+    email : req.body.email,
+    psd : req.body.psd
+})
 add.save()
 .then(()=>{ res.send({message :"user inserted Sucessfully"}); console.log("done")})
 .catch(e => res.send({message: 'Error'+e.message}))
 
 })
+
+router.post('/',async (req, res)=>
+{ console.log("login");
+
+const user=await Tododata.find({email : req.body.email})
+
+if(!user)
+{
+    return res.send(500,"no user");
+
+}
+const check = bcrypt.compareSync(req.body.psd,user[0].psd)
+
+if(!check)
+{
+    return res.send("500 ")
+}
+})
+
+
 
 module.exports=router;
